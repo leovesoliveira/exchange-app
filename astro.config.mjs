2b8ee44cfa56a9abf35fb8cabb5c2771fe29a515
Leonardo Alves <leoves.oliveira@gmail.com>
2023-10-31 18:15:46 -0300

    add service worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
+import serviceWorker from "astrojs-service-worker";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
+    serviceWorker(),
     tailwind({
       applyBaseStyles: false,
     }),
